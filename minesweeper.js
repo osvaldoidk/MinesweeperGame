6069f2d619060e7a878ca9849a811350aa941b8e
Osvaldo M <44970166+osvaldoidk@users.noreply.github.com>
2019-09-17 10:41:57 -0500

    Initial commit

---
+++ minesweeper.js
@@ -0,0 +1,20 @@
+/*
+Author: Osvaldo
+
+Terminal-based minesweeper game, requiring that you type code into the terminal to play the game
+*/
+
+let blankLine = '  |   |  ';
+
+console.log('This is what an empty board would look like: ');
+console.log(blankLine);
+console.log(blankLine);
+console.log(blankLine);
+
+let guessLine = '1 |   |  ';
+let bombLine = '  | B |  ';
+
+console.log('This is what a board with a guess and a bomb on it would look like:');
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine)
