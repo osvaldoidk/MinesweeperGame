605c31d79cb668721589b8cad8c00fa03d665355
Osvaldo M <44970166+osvaldoidk@users.noreply.github.com>
2019-09-17 11:59:01 -0500

    Create game board array

--- minesweeper.js
+++ minesweeper.js
@@ -4,17 +4,20 @@ Author: Osvaldo
 Terminal-based minesweeper game, requiring that you type code into the terminal to play the game
 */
 
-let blankLine = '  |   |  ';
+const board = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+];
 
-console.log('This is what an empty board would look like: ');
-console.log(blankLine);
-console.log(blankLine);
-console.log(blankLine);
+const printBoard = board => {
+    console.log(`Current Board: `);
+    console.log(board[0].join(' | '));
+    console.log(board[1].join(' | '));
+    console.log(board[2].join(' | '));
+};
 
-let guessLine = '1 |   |  ';
-let bombLine = '  | B |  ';
+printBoard(board);
 
-console.log('This is what a board with a guess and a bomb on it would look like:');
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine)
+board[2][2] = 'B';
+printBoard(board);
