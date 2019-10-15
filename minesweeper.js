13094df5b18b6028cc85597b0d5f5e2fcd06b7d1
Osvaldo M <44970166+osvaldoidk@users.noreply.github.com>
2019-10-15 16:32:21 -0500

    Dynamically generate game board

--- minesweeper.js
+++ minesweeper.js
@@ -4,20 +4,59 @@ Author: Osvaldo
 Terminal-based minesweeper game, requiring that you type code into the terminal to play the game
 */
 
-const board = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-];
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+    const board = [];
 
-const printBoard = board => {
-    console.log(`Current Board: `);
-    console.log(board[0].join(' | '));
-    console.log(board[1].join(' | '));
-    console.log(board[2].join(' | '));
+    for (let i = 0; i < numberOfRows; i++) {
+        const row = [];
+
+        for (let col = 0; col < numberOfColumns; col++) {
+            row.push(' ');
+        }
+        board.push(row);
+    }
+    return board;
 };
 
-printBoard(board);
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+    const board = [];
 
-board[2][2] = 'B';
-printBoard(board);
+    for (let i = 0; i < numberOfRows; i++) {
+        const row = [];
+
+        for (let col = 0; col < numberOfColumns; col++) {
+            row.push(null);
+        }
+
+        board.push(row);
+    }
+
+
+    let numberOfBombsPlaced = 0;
+
+    while (numberOfBombsPlaced < numberOfBombs) {
+        let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+        let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+
+        if (board[randomRowIndex][randomColumnIndex] === null) {
+            board[randomRowIndex][randomColumnIndex] = 'B';
+            numberOfBombsPlaced++;
+        }
+
+    }
+
+    return board;
+};
+
+const printBoard = (board) => {
+    console.log(board.map(row => row.join(' | ')).join('\n'));
+};
+
+let playerBoard = generatePlayerBoard(4, 8);
+let bombBoard = generateBombBoard(4, 8, 7);
+
+console.log('Player Board: ');
+printBoard(playerBoard);
+
+console.log('Bomb Board: ');
+printBoard(bombBoard);
